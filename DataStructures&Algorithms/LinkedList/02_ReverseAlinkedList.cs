2b2e135baf717b52826a513b0acc773be3694085
MonikaAherwar <monikaaherwar@gmail.com>
2023-02-17 01:06:41 +0530

    ReverseLinkedList

---
+++ DataStructures&Algorithms/LinkedList/02_ReverseAlinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.LinkedList
+{
+    internal class _02_ReverseAlinkedList
+    {
+        public static Node ReverseLinkedList(Node head)
+        {
+            Node prev = null;
+            while(head != null)
+            {
+                Node curr = head;
+                head = head.next;
+                curr.next = prev;
+                prev = curr;                
+            }
+
+            return prev;
+        }
+    }
+}
