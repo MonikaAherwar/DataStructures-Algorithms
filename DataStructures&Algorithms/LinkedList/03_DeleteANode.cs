c85c67383204b3cd250c55948cd7a694c2808ad4
MonikaAherwar <monikaaherwar@gmail.com>
2023-02-18 01:07:19 +0530

    DeleteNodeWithHeadPointer

---
+++ DataStructures&Algorithms/LinkedList/03_DeleteANode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.LinkedList
+{
+    internal class _03_DeleteANode
+    {
+        public void DeleteNodeWithHeadPointer(Node head)
+        {
+            int val = head.next.data;
+            head.next = head.next.next;
+            head.data = val;
+        }
+    }
+}
