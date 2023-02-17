cf465206e7bd25bee089040ae508a1172ec8ea35
MonikaAherwar <monikaaherwar@gmail.com>
2023-02-18 01:08:04 +0530

    FindMiddle Element

---
+++ DataStructures&Algorithms/LinkedList/05_FindMiddleElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DataStructures_Algorithms.LinkedList
+{
+    internal class _05_FindMiddleElement
+    {
+        public static Node MiddleElement(Node head)
+        {
+            if(head == null) return null;
+            Node slow = head;
+            Node fast = head;
+
+            while(fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next?.next;
+            }
+
+            return slow;
+        }
+    }
+}
