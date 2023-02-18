ef0ba6f9762bc0766bfac3f14bdf877c12d102a9
MonikaAherwar <monikaaherwar@gmail.com>
2023-02-19 01:08:33 +0530

    IsPalindrome

---
+++ DataStructures&Algorithms/LinkedList/06_CheckIfLinkedListIsPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.LinkedList
+{
+    internal class _06_CheckIfLinkedListIsPalindrome
+    {
+        public bool IsPalindrome(Node head)
+        {
+            Node mid = _05_FindMiddleElement.MiddleElement(head);
+            Node mid_reverse = _02_ReverseAlinkedList.ReverseLinkedList(mid.next);
+
+            bool isPalindrome = true;
+            while(mid != null && mid_reverse != null)
+            {
+                if(mid.data != mid_reverse.data)
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            mid.next = _02_ReverseAlinkedList.ReverseLinkedList(mid_reverse);
+            return isPalindrome;
+        }
+    }
+}
