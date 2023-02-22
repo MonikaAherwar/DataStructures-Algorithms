a6c188117897cd10dff4171848c0db07bc6ab55f
MonikaAherwar <monikaaherwar@gmail.com>
2023-02-23 01:10:34 +0530

    RotateByK

---
+++ DataStructures&Algorithms/LinkedList/11_RotateByKPlaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.LinkedList
+{
+    internal class _11_RotateByKPlaces
+    {
+        public Node RotateByK(Node head, int k)
+        {
+            if(head == null) return null;
+
+            Node prev = null;
+            Node curr = head;
+            while (k > 0 && curr != null)
+            {
+                Node temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+                k--;
+            }
+
+            head.next = RotateByK(curr, k);
+            return prev;
+        }
+    }
+}
