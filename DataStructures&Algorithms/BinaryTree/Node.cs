2333e14b8ef93dc447ab1bbf21fb3c1e9b4006cb
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-01 19:24:28 +0530

    Add Tree Node structure

---
+++ DataStructures&Algorithms/BinaryTree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinaryTree
+{
+    // Class containing left and right child
+    // of current node and key value
+
+    internal class Node
+    {
+        internal int Key;
+        internal Node Left;
+        internal Node Right;
+
+        public Node(int item)
+        {
+            Key = item;
+            Left = Right = null;
+        }
+    }
+}
