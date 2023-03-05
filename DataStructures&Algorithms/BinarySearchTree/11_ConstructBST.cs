dd7f54c6f3fbc94be64e24be9bc1056c66687315
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-06 00:55:44 +0530

    BST

---
+++ DataStructures&Algorithms/BinarySearchTree/11_ConstructBST.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearchTree
+{
+    internal class _11_ConstructBST
+    {
+        public Node SortedArrayToBST(int[] nums)
+        {
+
+            int n = nums.Length;
+            if (n == 0) return null;
+
+            return BST(nums, 0, n - 1);
+        }
+
+        private Node BST(int[] nums, int start, int end)
+        {
+
+            if (start > end) return null;
+
+            int mid = start + (end - start) / 2;
+            var node = new Node(nums[mid]);
+
+            node.Left = BST(nums, start, mid - 1);
+            node.Right = BST(nums, mid + 1, end);
+
+            return node;
+        }
+    }
+}
