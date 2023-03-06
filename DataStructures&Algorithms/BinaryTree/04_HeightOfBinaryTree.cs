6caea1e5f3bf4deaecb559372d0a0743bd8df71a
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-06 20:39:30 +0530

    Height

---
+++ DataStructures&Algorithms/BinaryTree/04_HeightOfBinaryTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinaryTree
+{
+    internal class _04_HeightOfBinaryTree
+    {
+        public int Height(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            int left = Height(root.Left);
+            int right = Height(root.Right); 
+            int height = Math.Max(left, right) + 1;
+            return height;
+        }
+    }
+}
