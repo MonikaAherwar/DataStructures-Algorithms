06d7d2f8981e4df3164d99836a15c46e26ad817f
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-07 21:29:51 +0530

    DiameterOfBinaryTree

---
+++ DataStructures&Algorithms/BinaryTree/05_DiameterOfBinaryTee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinaryTree
+{
+    internal class _05_DiameterOfBinaryTee
+    {
+        internal int diameter = 0;
+        public int DiameterOfBinaryTree(Node root)
+        {
+            int diameter = 0;
+            Height(root, ref diameter);
+            return diameter;
+        }
+
+        private int Height(Node root, ref int diameter)
+        {
+            if (root == null) 
+                return 0;
+
+            var left_height = Height(root.Left, ref diameter);
+            var right_height = Height(root.Right, ref diameter);
+            var current_height = 1 + Math.Max(left_height, right_height);
+
+            int diameterWhenCurrentNodeIsIncluded = left_height + right_height + 1;
+            diameter = Math.Max(diameter, diameterWhenCurrentNodeIsIncluded);
+
+            return current_height;
+        }
+    }
+}
