c705b09fb677835ca227279ba3da771aae31ffea
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-08 21:30:10 +0530

    IsIdentical

---
+++ DataStructures&Algorithms/BinaryTree/06_IsIdenticalTree.cs
@@ -0,0 +1,25 @@
+﻿using DataStructures_Algorithms.SlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinaryTree
+{
+    internal class _06_IsIdenticalTree
+    {
+
+        public static bool IsIdentical(Node root1, Node root2)
+        {
+            if (root1 == null && root2 == null)
+                return true;
+
+            if (root1 == null || root2 == null)
+                return false;
+
+            // Case: When root1 != NULL && root2 != NULL
+            return (root1.Key == root2.Key)
+                && IsIdentical(root1.Left, root2.Left)
+                && IsIdentical (root1.Right, root2.Right);
+        }
+    }
+}
