cf81fc28a35118fbd77009cb8287efc1797b44f9
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-10 00:56:07 +0530

    LCA

---
+++ DataStructures&Algorithms/BinarySearchTree/12_LCA.cs
@@ -0,0 +1,26 @@
+﻿using DataStructures_Algorithms.SlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearchTree
+{
+    internal class _12_LCA
+    {
+        bool found1 = false;
+        bool found2 = false ;
+        public Node LCA(Node root, Node p, Node q)
+        {
+            if (root == null) return null;
+
+            if (root.Key > p.Key && root.Key > q.Key)
+                return LCA(root.Left, p, q);
+
+            if (root.Key < p.Key && root.Key < q.Key)
+                return LCA(root.Right, p, q);
+
+            return root;
+        }
+    }
+}
