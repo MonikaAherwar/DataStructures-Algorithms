ea84cb9e9c6fdcfdd3a4b698ae2f6b183d60a1a7
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-10 21:30:52 +0530

    IsSubtree

---
+++ DataStructures&Algorithms/BinaryTree/08_IsSubtree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinaryTree
+{
+    internal class _08_IsSubtree
+    {
+        public bool IsSubtree(Node t1, Node t2)
+        {
+            if (t1 == null)
+                return false;
+
+            if (t2 == null)
+                return true;
+
+            if (t1.Key == t2.Key)
+            {
+                return _06_IsIdenticalTree.IsIdentical(t1, t2);
+            }
+
+            return IsSubtree(t1.Left, t2) || IsSubtree(t1.Right, t2);          
+        }
+    }
+}
