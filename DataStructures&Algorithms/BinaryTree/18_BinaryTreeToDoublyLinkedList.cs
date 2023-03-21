f27867f14078bfdaecd65402c671717ed215cb48
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-22 00:59:54 +0530

    BinaryTree2DoubleLinkedList

--- DataStructures&Algorithms/BinaryTree/18_BinaryTreeToDoublyLinkedList.cs
+++ DataStructures&Algorithms/BinaryTree/18_BinaryTreeToDoublyLinkedList.cs
@@ -6,9 +6,42 @@ namespace DataStructures_Algorithms.BinaryTree
 {
     internal class _18_BinaryTreeToDoublyLinkedList
     {
-        public void DoublyLinkedList(Node root)
+
+        // Recursive Way
+
+        public static Node prev = null;
+        public Node head;
+        public void BinaryTree2DoubleLinkedList(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            // Recursively convert left subtree 
+            BinaryTree2DoubleLinkedList(root.Left);
+
+            // Now convert this node 
+            if (prev == null)
+            {
+                head = root;
+            }
+            else
+            {
+                root.Left = prev;
+                prev.Right = root;
+            }
+            prev = root;
+
+            // Finally convert right subtree 
+            BinaryTree2DoubleLinkedList(root.Right);
+        }
+
+        // Iterative way: Morris Travel
+        public void BinaryTree2DoubleLinkedList_Iterative(Node root)
         {
 
         }
+
     }
 }
