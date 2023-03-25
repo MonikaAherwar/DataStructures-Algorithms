89ff6673d7e963e30f283f738fce9e8c87bf02e0
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-26 00:50:27 +0530

    ArrayToBST

---
+++ DataStructures&Algorithms/BinarySearchTree/02_ArrayToBST.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearchTree
+{
+    internal class _02_ArrayToBST
+    {
+        public Node CreateBalancedBinaryTreeFromSortedArray(int[] arr, int start, int end) {
+
+            Node node = null;
+            if(start > end) return node;
+
+            //if (n == 1) return new Node(arr[start]);
+
+            if(start <= end)
+            {
+                int mid = start + (end - start) / 2;
+
+                node = new Node(arr[mid]);
+
+                node.Left = CreateBalancedBinaryTreeFromSortedArray(arr, start, mid - 1);
+                node.Right = CreateBalancedBinaryTreeFromSortedArray(arr, mid + 1, end);
+
+            }
+
+            return node;
+        }
+    }
+}
