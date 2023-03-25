e91ed60ed7ca9ee138b59cd28510666c9989a96d
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-26 00:51:20 +0530

    Node

---
+++ DataStructures&Algorithms/BinarySearchTree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearchTree
+{
+    internal class Node
+    {
+        internal int Key;
+        internal Node Left;
+        internal Node Right;
+
+        public Node(int item)
+        {
+            Key = item;
+            Left = Right = null;
+        }
+    }
+}
