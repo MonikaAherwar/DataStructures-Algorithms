9d487a617b4ae1bd01b015c958b83730c230f35f
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-27 00:51:45 +0530

    KthLargestElement

---
+++ DataStructures&Algorithms/BinarySearchTree/04_KthLargestElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DataStructures_Algorithms.BinarySearchTree
+{
+    internal class _04_KthLargestElement
+    {
+        static int count = 0; 
+        public void KthLargestElement(Node root, int k)
+        {
+            
+            // Reverse Inorder Traversal
+            // LVR --> RVL
+
+            if(root != null && k > 0)
+            {
+                KthLargestElement(root.Right, k);
+
+                count++;
+                if (k == count)
+                {
+                    Console.WriteLine(root.Key);
+                    return;
+                }
+
+                KthLargestElement(root.Left, k--);
+            }
+        }
+    }
+}
