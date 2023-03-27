911c64ed58b42211141115489d9165bfd29b2923
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-28 00:52:08 +0530

    RemoveBSTKeysOutsideRange

---
+++ DataStructures&Algorithms/BinarySearchTree/05_RemoveBSTKeysOutsideGivenRange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearchTree
+{
+    internal class _05_RemoveBSTKeysOutsideGivenRange
+    {
+        public Node RemoveBSTKeysOutsideRange(Node root, int min, int max)
+        {
+            if(root == null) return null;
+
+            root.Left = RemoveBSTKeysOutsideRange(root.Left, min, max);
+
+            root.Right = RemoveBSTKeysOutsideRange(root.Right, min, max);
+
+            if (root.Key < min)
+            {
+                root = root.Right;
+            }
+            else if (root.Key > max)
+            {
+                root = root.Left;
+            }
+
+            return root;
+        }
+    }
+}
