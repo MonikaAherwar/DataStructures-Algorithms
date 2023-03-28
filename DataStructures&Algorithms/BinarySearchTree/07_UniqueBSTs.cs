59cd709930ce0c452f94d5522a999a3120e1cdf1
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-29 00:52:59 +0530

    UniqueBSTs

---
+++ DataStructures&Algorithms/BinarySearchTree/07_UniqueBSTs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearchTree
+{
+    internal class _07_UniqueBSTs
+    {
+
+        // For N = 3
+        // OP: 5
+        static int numberOfUniqueBST(int n)
+        {
+
+            // DP to store the number
+            // of unique BST with key i
+            int[] dp = new int[n + 1];
+
+            // Base case
+            dp[0] = 1;
+            dp[1] = 1;
+
+            // fill the dp table in
+            // top-down approach.
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+
+                    // n-i in right * i-1 in left
+                    dp[i] = dp[i] + (dp[i - j] * dp[j - 1]);
+                }
+            }
+            return dp[n];
+        }
+    }
+}
