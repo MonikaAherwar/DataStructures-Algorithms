95599ec6b03c11f4d9f322b382222c0a0cb44262
MonikaAherwar <monikaaherwar@gmail.com>
2023-03-30 00:54:00 +0530

    MergeTwoBSTs

---
+++ DataStructures&Algorithms/BinarySearchTree/09_MergeTwoBSTs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearchTree
+{
+    internal class _09_MergeTwoBSTs
+    {
+        List<int> list = new List<int>();
+        public Node MergeTwoBSTs(Node root1, Node root2)
+        {
+            if (root1 == null) return root2;
+
+            if (root2 == null) return root1;
+
+            // May be later
+        }
+    }
+}
