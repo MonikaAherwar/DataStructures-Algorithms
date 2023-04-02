045c239496cf53e6e7c667bf6dbd722326b1de37
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-03 01:45:24 +0530

    01_KnapSack_Recursive

---
+++ DataStructures&Algorithms/DP/01_01KnapSack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.DP
+{
+    internal class _01_01KnapSack
+    {
+        // wt[]: Weight array
+        // val[]: value array
+        // W: Total target weight
+        // n: no. of elements in wt, val array
+
+        public int _01_KnapSack_Recursive(int[] wt, int[] val, int W, int n)
+        {
+            if(W == 0 || n == 0)
+            {
+                return 0;
+            }
+
+            int value = 0;
+            if (wt[n-1] <= W)
+            {
+                value = Math.Max(val[n - 1] + _01_KnapSack_Recursive(wt, val, W - val[n - 1], n - 1),
+                    _01_KnapSack_Recursive(wt, val, W, n - 1));
+            }
+            else if (wt[n-1] > W)
+            {
+                value = _01_KnapSack_Recursive(wt, val, W, n - 1);
+            }
+
+            return value;
+        }
+    }
+}
