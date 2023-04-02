8b4f162091ca59d22b3fd91843d80524a06976a1
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-03 02:02:34 +0530

    01_01KnapSack_Memoised

--- DataStructures&Algorithms/DP/01_01KnapSack.cs
+++ DataStructures&Algorithms/DP/01_01KnapSack.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures_Algorithms.SlidingWindow;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -31,5 +32,39 @@ namespace DataStructures_Algorithms.DP
 
             return value;
         }
+
+
+        //*********
+        // Memoised
+        //*********
+
+        int[,] t = DP_Helper.FillDefault(m: 4, n: 7);
+        
+        public int _01_KnapSack_Memoised(int[] wt, int[] val, int W, int n)
+        {
+            if (W == 0 || n == 0)
+            {
+                return 0;
+            }
+
+            if (t[n, W] != -1)
+                return t[n, W];
+
+            int value = 0;
+            if (wt[n - 1] <= W)
+            {
+                value = Math.Max(val[n - 1] + _01_KnapSack_Memoised(wt, val, W - val[n - 1], n - 1),
+                    _01_KnapSack_Memoised(wt, val, W, n - 1));
+            }
+            else if (wt[n - 1] > W)
+            {
+                value = _01_KnapSack_Memoised(wt, val, W, n - 1);
+            }
+
+            t[n, W] = value;
+            return t[n, W];
+        }
+
+        
     }
 }
