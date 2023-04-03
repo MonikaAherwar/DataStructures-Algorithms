785c16796d707b889ede706b612f25c3e09998c7
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-04 02:41:18 +0530

    01_01KnapSack_TopDown

--- DataStructures&Algorithms/DP/01_01KnapSack.cs
+++ DataStructures&Algorithms/DP/01_01KnapSack.cs
@@ -68,6 +68,49 @@ namespace DataStructures_Algorithms.DP
             return t[n, W];
         }
 
-        
+        //**********
+        // Top Down
+        //**********
+
+        // n = 4
+        // W = 7
+        //int[,] dp = new dp[n+1, W+1]; 
+        int[,] dp = DP_Helper.FillDefault(m: 5, n: 8);
+
+        public int _01_KnapSack_TopDown(int[] wt, int[] val, int W, int n)
+        {
+            //Base condition
+            int i;
+            int j;
+            for(i = 0; i < n+1; i++)
+            {
+                for(j = 0; j < W+1; j++)
+                {
+                    if(i == 0 || j == 0)
+                    {
+                        dp[i, j] = 0;
+                    }
+                }
+            }
+
+            //Choise Diagram
+
+            for (i = 1; i < n + 1; i++)
+            {
+                for (j = 1; j < W + 1; j++)
+                {
+                    if (wt[i-1] < W)
+                    {
+                        dp[i,j] = Math.Max(val[i-1] + dp[i-1, j-wt[i-1]], dp[i-1, j]);
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i - 1, j];
+                    }
+                }
+            }
+
+            return dp[n+1, W+1];
+        }
     }
 }
