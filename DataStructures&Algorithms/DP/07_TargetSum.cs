9554316b75bbc6b3df2fd23a712a5fa78c18b1b8
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-12 03:48:24 +0530

    TargetSum

---
+++ DataStructures&Algorithms/DP/07_TargetSum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.DP
+{
+    internal class _07_TargetSum
+    {
+        public int TargetSum(int[] arr, int sum, int n)
+        {
+
+            // Problem is reduded to CountSubsetWithGivenDiff
+            int diff = sum;
+            var count = _06_CountSubsetWithGivenDiff.CountSubsetWithGivenDiff(arr, n, diff);
+            return count;
+        }
+    }
+}
