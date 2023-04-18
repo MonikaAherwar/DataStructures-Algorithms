42b811387479ea3a0941c314128e5ddd09519167
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-18 20:51:23 +0530

    LCS Memoised

--- DataStructures&Algorithms/DP/LongestCommomSubsequence/01_LongestCommomSubsequence.cs
+++ DataStructures&Algorithms/DP/LongestCommomSubsequence/01_LongestCommomSubsequence.cs
@@ -31,6 +31,41 @@ namespace DataStructures_Algorithms.DP.LongestCommomSubsequence
         }
 
 
+        //***********
+        // Memoised
+        //***********
+
+        //m: 6
+        //n: 5
+        int[,] t = new int[7, 6];
+        public int LongestCommomSubsequence_Memoised(string X, string Y, int n, int m)
+        {
+            for(int i = 0; i <= m; i++)
+            {
+                for(int j = 0; j <= n; j++)
+                {
+                    t[i, j] = -1;
+                }
+            }
+
+            if (m == 0 || n == 0) return 0;
+
+            if (t[m, n] != -1) return t[m, n];
+
+            if (X[m - 1] == Y[n - 1])
+            {
+                t[m, n] = 1 + LongestCommomSubsequence_Recursive(X, Y, m - 1, n - 1);
+            }
+            else
+            {
+                t[m, n] = Math.Max(LongestCommomSubsequence_Recursive(X, Y, m - 1, n),
+                    LongestCommomSubsequence_Recursive(X, Y, m, n - 1));
+            }
+
+            return t[m, n];
+        }
+
+
         
     }
 }
