c727ee08339ab51f18a572693239a08878a3dbe6
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-22 02:46:26 +0530

    MinimumInsertionAndDeletion

---
+++ DataStructures&Algorithms/DP/LongestCommomSubsequence/05_MinimumInsertionAndDeletion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.DP.LongestCommomSubsequence
+{
+    internal class _05_MinimumInsertionAndDeletion
+    {
+        public int MinimumInsertionAndDeletion(string X, string Y, int m, int n)
+        {
+            int lcs = _01_LongestCommomSubsequence.LongestCommomSubsequence_TopDown(X, Y, m, n);
+            int deletion = m - lcs;
+            int insertion = n - lcs;
+
+            return insertion + deletion;
+        }
+    }
+}
