6aeb89fff07dcc6dedca949bc8c6f514ae90b459
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-23 02:46:58 +0530

    LongestPalindromicSubstring

---
+++ DataStructures&Algorithms/DP/LongestCommomSubsequence/06_LongestPalindromicSubstring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructures_Algorithms.DP.LongestCommomSubsequence
+{
+    internal class _06_LongestPalindromicSubstring
+    {
+        // X = agbcba
+        // OP: 5
+        public static int LongestPalindromicSubstring(string X, int m)
+        {
+            string Y = X.Reverse().ToString();
+            int n = m;
+
+            int lcs = _01_LongestCommomSubsequence.LongestCommomSubsequence_TopDown(X, Y, m, n);
+
+            return lcs;
+        }
+    }
+}
