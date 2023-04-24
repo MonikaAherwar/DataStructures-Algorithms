a93714756bfe3c109cdcd415d2108eccdef63d50
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-25 02:47:28 +0530

    MinimumDeletion

---
+++ DataStructures&Algorithms/DP/LongestCommomSubsequence/07_MinimumDeletionToMakeAstringPalindrome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.DP.LongestCommomSubsequence
+{
+    internal class _07_MinimumDeletionToMakeAstringPalindrome
+    {
+        public static int MinimumDeletion(string X, int m)
+        {
+            int longestPalindromeSubstring = _06_LongestPalindromicSubstring.LongestPalindromicSubstring(X, m);
+            int minDeletion = m - longestPalindromeSubstring;
+            return minDeletion;
+        }
+    }
+}
