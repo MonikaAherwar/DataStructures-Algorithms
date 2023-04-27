fc8b36a54707a2be6dbdf8ef49f313d8b8309ca4
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-28 02:48:41 +0530

    MinimumInsetion

---
+++ DataStructures&Algorithms/DP/LongestCommomSubsequence/10_MinimumInsertionToMakeAstringPalindrome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.DP.LongestCommomSubsequence
+{
+    internal class _10_MinimumInsertionToMakeAstringPalindrome
+    {
+        public int MinimumInsetion(String X, int m)
+        {
+            // No. of insertion =  No. of deletion
+
+            int minInsetion = _07_MinimumDeletionToMakeAstringPalindrome.MinimumDeletion(X, m);
+            return minInsetion;
+        }
+    }
+}
