a1965e18f5d905f944c3e797b87919d6d559b3ef
MonikaAherwar <monikaaherwar@gmail.com>
2023-04-28 02:48:15 +0530

    SequencePatternMatching

---
+++ DataStructures&Algorithms/DP/LongestCommomSubsequence/09_SequencePatternMatching.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.DP.LongestCommomSubsequence
+{
+    internal class _09_SequencePatternMatching
+    {
+        // X: AXY
+        // Y: ADXCPY
+        // Problem: Does X lies in Y
+        // OP: True
+        public bool SequencePatternMatching(String X, String Y, int m, int n)
+        {
+            int lcs = _01_LongestCommomSubsequence.LongestCommomSubsequence_TopDown(X, Y, m, n);    
+
+            if(lcs == Math.Min(m, n))
+                return true;
+
+            return false;
+        }
+    }
+}
