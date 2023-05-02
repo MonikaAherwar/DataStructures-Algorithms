1e6fc8732d22d890fd0befb388c627ba6a7f6ae2
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-03 01:44:58 +0530

    Add DP_Helper

---
+++ DataStructures&Algorithms/DP/DP_Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.DP
+{
+    internal class DP_Helper
+    {
+        public int[,] FillDefault(int[,] dp)
+        {
+            int m = dp.GetLength(0);
+            int n = dp.GetLength(1);
+
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    dp[i, j] = -1;
+                }
+            }
+
+            return dp;
+        }
+    }
+}
