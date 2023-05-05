1b856dd27070fa57ba45e6d5563757f2eb6961c8
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-05 19:08:11 +0530

    NSR

---
+++ DataStructures&Algorithms/Stack/04_NSR_NextSmallestRight.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.Stack
+{
+    internal class _04_NSR_NextSmallestRight
+    {
+
+        // Nearest Smallest Element Right
+        public List<int> NSR(int[] arr)
+        {
+            int n = arr.Length;
+
+            if(n == 0)
+            {
+                throw new ArgumentException("Array is Empty");
+            }
+
+            List<int> result = new List<int>();
+            Stack<int> stack = new Stack<int>();
+
+            for(int i = n-1; i >= 0; i--)
+            {
+                if (stack.Count == 0)
+                {
+                    result.Add(-1);
+                }
+                else if (stack.Count > 0 && stack.Peek() < arr[i])
+                {
+                    result.Add(stack.Peek());
+                }
+                else
+                {
+                    while(stack.Count > 0 && stack.Peek() >= arr[i])
+                    {
+                        stack.Pop();
+                    }
+
+                    if(stack.Count == 0)
+                    {
+                        result.Add(-1);
+                    }
+                    else
+                    {
+                        result.Add(stack.Peek());
+                    }
+                }
+
+                stack.Push(arr[i]);
+            }
+
+            //Reverse the List
+            result.Reverse();
+            return result;
+        }
+
+    }
+}
