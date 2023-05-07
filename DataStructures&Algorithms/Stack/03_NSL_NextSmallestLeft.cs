14a4bb0e6eff193f65427415b0ff9b30baf0cd7c
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-07 21:44:04 +0530

    Add code NSL_Helper

--- DataStructures&Algorithms/Stack/03_NSL_NextSmallestLeft.cs
+++ DataStructures&Algorithms/Stack/03_NSL_NextSmallestLeft.cs
@@ -53,5 +53,56 @@ namespace DataStructures_Algorithms.Stack
             return result;
         }
 
+        public static List<int> NSL_Helper(int[] arr)
+        {
+            int n = arr.Length;
+
+            if (n == 0)
+            {
+                throw new ArgumentException("Array is Empty");
+            }
+
+            List<int> result = new List<int>();
+            Stack<StackHelper> stack = new Stack<StackHelper>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (stack.Count == 0)
+                {
+                    result.Add(-1);
+                }
+                else if (stack.Count > 0 && stack.Peek().Element < arr[i])
+                {
+                    result.Add(stack.Peek().Index);
+                }
+                else
+                {
+                    while (stack.Count > 0 && stack.Peek().Element >= arr[i])
+                    {
+                        stack.Pop();
+                    }
+
+                    if (stack.Count == 0)
+                    {
+                        result.Add(-1);
+                    }
+                    else
+                    {
+                        result.Add(stack.Peek().Index);
+                    }
+                }
+
+                stack.Push(new StackHelper { Element = arr[i], Index = i });
+            }
+
+            return result;
+        }
+
+        internal class StackHelper
+        {
+            internal int Element;
+            internal int Index;
+        }
+
     }
 }
