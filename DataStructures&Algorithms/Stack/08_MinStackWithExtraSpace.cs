a69f317edf2108dc0de4e193ac87e8c22f2d4a1c
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-11 23:56:54 +0530

    GetMinStackWithExtraSpace

---
+++ DataStructures&Algorithms/Stack/08_MinStackWithExtraSpace.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DataStructures_Algorithms.Stack
+{
+    internal class _08_MinStackWithExtraSpace
+    {
+        Stack<int> stack = new Stack<int> ();
+        Stack<int> supportingStack = new Stack<int> ();
+
+        public void Push(int ele)
+        {
+            if(supportingStack.Count == 0)
+            {
+                supportingStack.Push (ele);
+            }
+            else if(ele <= supportingStack.Peek())
+            {
+                supportingStack.Push (ele);
+            }
+
+            stack.Push (ele);
+        }
+
+        public void Pop(int ele)
+        {
+            if(supportingStack.Count > 0 && supportingStack.Peek() == ele)
+            {
+                supportingStack.Pop ();
+            }
+
+            stack.Pop ();
+        }
+
+        public int GetMin()
+        {
+            if(supportingStack.Count == 0)
+            {
+                throw new Exception("Stack is Empty");
+            }
+
+            return supportingStack.Peek();
+        }
+    }
+}
