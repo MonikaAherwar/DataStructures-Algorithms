00464322f3fed0794dd541a7c15fb88de75b377d
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-13 01:38:59 +0530

    Stable SelectionSort

--- DataStructures&Algorithms/SortingAlgorithms/01_SelectionSort.cs
+++ DataStructures&Algorithms/SortingAlgorithms/01_SelectionSort.cs
@@ -35,5 +35,43 @@ namespace DataStructures_Algorithms.SortingAlgorithms
                 arr[min_Index] = temp;
             }
         }
+
+
+        // O(N^2)
+        // In Place
+        // Not Stable
+        public void SelectionSortAlgorithm_Stable(int[] arr)
+        {
+            int n = arr.Length;
+            if (n == 0)
+            {
+                throw new ArgumentException("Array is Empty");
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int min_Index = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min_Index])
+                    {
+                        min_Index = j;
+                    }
+                }
+
+                // Instead swap the min element with index i
+                // We will slide the elements from i to min index
+                // This will maintain the order of elements 
+                // Hence stable
+
+                int min_ele = arr[min_Index];
+                for (int k = min_Index; k > i; k--)
+                {
+                    arr[k] = arr[k - 1];
+                }
+
+                arr[i] = min_ele;
+            }
+        }
     }
 }
