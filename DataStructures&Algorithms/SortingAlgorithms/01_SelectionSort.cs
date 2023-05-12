a7ca31ddfa1adca73351114e4165022607a3f85b
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-13 01:38:22 +0530

    SelectionSort

---
+++ DataStructures&Algorithms/SortingAlgorithms/01_SelectionSort.cs
@@ -0,0 +1,39 @@
+﻿using DataStructures_Algorithms.SlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.SortingAlgorithms
+{
+    internal class SelectionSort
+    {
+        // O(N^2)
+        // In Place
+        // Not Stable
+        public void SelectionSortAlgorithm(int[] arr)
+        {
+            int n = arr.Length;
+            if(n == 0)
+            {
+                throw new ArgumentException("Array is Empty");
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                int min_Index = i;
+                for(int j = i+1; j < n; j++)
+                {
+                    if (arr[j] < arr[min_Index])
+                    {
+                        min_Index = j;                     
+                    }
+                }  
+                
+                //Swap
+                int temp = arr[i];
+                arr[i] = arr[min_Index];
+                arr[min_Index] = temp;
+            }
+        }
+    }
+}
