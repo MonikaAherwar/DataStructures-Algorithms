60e4b9959a10b1065f40fbfb3bee8b493a41f183
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-14 01:48:38 +0530

    BubbleSortAlgorithm

---
+++ DataStructures&Algorithms/SortingAlgorithms/02_BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.SortingAlgorithms
+{
+    internal class BubbleSort
+    {
+
+        // O(N^2)
+        // In Place
+        // Stable
+        public void BubbleSortAlgorithm(int[] arr)
+        {
+            int n = arr.Length;
+            if (n == 0)
+            {
+                throw new ArgumentException("Array is Empty");
+            }
+
+            bool swapped;
+            for(int i = 0; i < n; i++)
+            {
+                swapped = false;
+                for(int j = 0; j < n-i-1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        //Swap
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                    break;
+            }        
+        }
+    }
+}
