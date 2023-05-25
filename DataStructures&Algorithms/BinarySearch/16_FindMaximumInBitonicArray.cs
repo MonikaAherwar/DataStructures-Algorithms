40fecabe47c836fbee8f20bb57eaea33002c7a13
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-25 23:51:34 +0530

    FindMaximumElementArrayInBitonicArray

---
+++ DataStructures&Algorithms/BinarySearch/16_FindMaximumInBitonicArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearch
+{
+    internal class _16_FindMaximumInBitonicArray
+    {
+
+        // Binary Search on concept
+        public int FindMaximumElementArrayInBitonicArray(int[] arr) {
+
+            int n = arr.Length;
+            int start = 0;
+            int end = n - 1;
+            int index = -1;
+
+            if (n == 0)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            int peakIndex = _15_FindPeakElement.FindPeakElementIndex(arr);
+
+            return peakIndex >= 0 ? arr[peakIndex] : -1;
+        }
+    }
+}
