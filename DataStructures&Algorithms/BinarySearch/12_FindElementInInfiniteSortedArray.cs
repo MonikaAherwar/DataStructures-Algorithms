14672e946c3154fbe0dcac6b647ff5c18245e5cb
MonikaAherwar <monikaaherwar@gmail.com>
2023-05-31 21:27:13 +0530

    FindElementInInfiniteSortedArray

---
+++ DataStructures&Algorithms/BinarySearch/12_FindElementInInfiniteSortedArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearch
+{
+    internal class _12_FindElementInInfiniteSortedArray
+    { 
+        public static int FindElementInInfiniteSortedArray(int[] arr, int ele) { 
+                      
+            int start = 0;
+            int end = start+1;
+
+            while(ele > arr[end])
+            {
+                start = end;
+                end = end * 2;
+            }
+
+            return _01_BinarySearch.BinarySearchAlgorithm(arr, ele, start, end);
+            
+        }
+    }
+}
