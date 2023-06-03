67dd51a512734d5a31866012fdb70e9d856aed42
MonikaAherwar <monikaaherwar@gmail.com>
2023-06-03 17:21:13 +0530

    BinarySearchAlgorithmInSortedArray

---
+++ DataStructures&Algorithms/BinarySearch/03_AgnosticSearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearch
+{
+    internal class _03_AgnosticSearch
+    {
+        //Agnostic Array: When sorting order is not known for the given array.
+        public int BinarySearchAlgorithmInSortedArray(int[] arr, int ele) { 
+            
+            int n = arr.Length;
+
+            if (n == 0) {
+                throw new ArgumentException("Array is empty");
+            }
+            
+            if(n == 1 && arr[0] == ele) 
+            {
+                return 0;
+            }
+
+            // Sorted Array
+            if (arr[0] < arr[1])
+            {
+                return _01_BinarySearch.BinarySearchAlgorithm(arr, n);
+            }
+            // Reverse sorted Array
+            else
+            { 
+                return _02_BinarySearchInReverseArray.BinarySearchAlgorithmInReverseSortedArray(arr, n);
+            }
+        }
+    }
+}
