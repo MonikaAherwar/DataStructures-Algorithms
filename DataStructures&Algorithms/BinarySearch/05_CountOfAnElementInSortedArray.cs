dfd316450f394747dc70771c723979ae43d23697
MonikaAherwar <monikaaherwar@gmail.com>
2023-06-15 17:42:27 +0530

    CountOfElement

---
+++ DataStructures&Algorithms/BinarySearch/05_CountOfAnElementInSortedArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.BinarySearch
+{
+    internal class _05_CountOfAnElementInSortedArray
+    {
+
+        public int CountOfElement(int[] arr, int ele) {
+
+            int count = 0;
+
+            int first = _04_FirstOccurenceOfElement.FirstOccurence(arr, ele);
+            int last = _04_LastOccurenceOfElement.LastOccurence(arr, ele);
+
+            count = last - first + 1;
+
+            return count;
+        }  
+    }
+}
