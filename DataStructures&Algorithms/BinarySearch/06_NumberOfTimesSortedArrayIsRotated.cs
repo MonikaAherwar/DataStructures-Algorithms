3724f238ee8092c19ebe0b2158faab8ffbea5c6e
MonikaAherwar <monikaaherwar@gmail.com>
2023-06-16 19:47:35 +0530

    Update code PivotInRotatedArray

--- DataStructures&Algorithms/BinarySearch/06_NumberOfTimesSortedArrayIsRotated.cs
+++ DataStructures&Algorithms/BinarySearch/06_NumberOfTimesSortedArrayIsRotated.cs
@@ -8,7 +8,7 @@ namespace DataStructures_Algorithms.BinarySearch
     {
 
         // This does not handle duplicates
-        public static int PivotInRotatedArray(int[] arr, int ele, bool rightRotatedArray) { 
+        public static int PivotInRotatedArray(int[] arr, bool rightRotatedArray) { 
             
             int n = arr.Length;
             int start = 0;
