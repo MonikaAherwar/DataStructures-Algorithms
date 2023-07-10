d7afdc6fef19480b64df7bd86a3a7b1d841cc18b
MonikaAherwar <monikaaherwar@gmail.com>
2023-07-10 22:31:50 +0530

    LargestSubarraySumForPositiveNumbers

---
+++ DataStructures&Algorithms/SlidingWindow/LargestSubarraySumForPositiveNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures_Algorithms.SlidingWindow
+{
+    internal class LargestSubarraySumForPositiveNumbers
+    {
+        int windowSize = 0;
+
+        public int LargestSubarraySum(int[] arr, int sum) {
+
+            int currSum = 0;
+            int i = 0;
+            int j = 0;
+            int n = arr.Length;
+
+            while (j < n) { 
+                
+                currSum += arr[j];
+
+                if (currSum < sum)
+                {
+                    j++;
+                }
+                else if (currSum == sum)
+                {
+                    windowSize = Math.Max(windowSize, currSum);
+                    j++;
+                }
+                else if (currSum > sum) {
+                    while (currSum > sum) {
+                        sum = sum - arr[i];
+                        i++;
+                    }
+                    j++;
+                }
+            }
+            
+            return windowSize;
+        }
+    }
+}
