2fe1927acf604a0dc005f67b1c5ca4782110dc9d
MonikaAherwar <monikaaherwar@gmail.com>
2023-07-26 03:02:40 +0530

    Test Trie

--- DataStructures&Algorithms/Program.cs
+++ DataStructures&Algorithms/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructures_Algorithms.Trie;
+using System;
+using System.Globalization;
 
 namespace DataStructures_Algorithms
 {
@@ -7,6 +9,25 @@ namespace DataStructures_Algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //InitializeTrieNode();
+            
+        }
+
+        private static void InitializeTrieNode()
+        {
+            TrieNode root = new TrieNode(26);
+            string[] strs = { "flower", "flow", "flight" };
+
+            foreach (string str in strs)
+            {
+                TrieNode.Insert(root, str);
+            }
+
+            foreach (string str in strs)
+            {
+                var val = TrieNode.Search(root, str);
+                Console.WriteLine(val);
+            }
         }
     }
 }
